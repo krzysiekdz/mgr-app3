fab08bd46a672852935660c2298094319681ad56
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-08-07 21:30:48 +0200

    updates...

--- src/script.js
+++ src/script.js
@@ -5,116 +5,52 @@ var results = {};
 
 app.controller('mainCtrl', ['$scope', function(s) {
 	s.results = results;
+	s.flags = {};
+	s.flags.showDetailedKeyed = false;
+	s.flags.showGeneralKeyed = false;
+	s.flags.showMemoryKeyed = false;
+	s.flags.showKeyed = true;
+
+	s.flags.showDetailedNonk = false;
+	s.flags.showGeneralNonk = false;
+	s.flags.showMemoryNonk = false;
+	s.flags.showNonk = true;
+
 }]);
 
 app.run(['$http', function(http) {
 	http({
 		method: 'GET',
-		url: 'http://localhost:8080/webdriver/app3/results.json'
+		url: './results.json'
 	}).then(function(data) {
 		results.data = data.data;
 		// console.log(data);
 	});
 }]);
 
-app.filter('groupByFramework_nonk', function() {//grouping by framework non-keyed and returning  predefined order
-	var res = [];
-	
-	return function(arr) {
-		if(arr === undefined)
-			return [];
-		var cats = {};
-		var name;
-		for(var i = 0; i < arr.length; i++) {
-			var item = arr[i];
-			if(item.keyed || item.summary) //only non-keyed results
-				continue;
-			name = item['framework'];
-			if(!cats.hasOwnProperty(name)) {
-				cats[name] = [item];
-				cats[name].sort_index = item.framework_index;
-			} else {
-				cats[name].push(item);
-			}
-		}
-		
-		var tmp = [];
-		for(var prop in cats) {
-			var sort_index = cats[prop].sort_index;
-			delete cats[prop].sort_index;
-			tmp.push({
-				key: prop,
-				value: cats[prop],
-				sort_index: sort_index,
-			});
-		}
-		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
 
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
-		}
-
-		return res[0];
-		// return tmp;
-	};
-});
-
-app.filter('groupByBenchmark_nonk', function() {//grouping by benchmark non-keyed and returning  predefined order
-	var res = [];
-	
-	return function(arr) {
-		if(arr === undefined)
-			return [];
-		var cats = {};
-		var name;
-		for(var i = 0; i < arr.length; i++) {
-			var item = arr[i];
-			if(item.keyed || item.summary) //only non-keyed results
-				continue;
-			name = item['benchmark'];
-			if(!cats.hasOwnProperty(name)) {
-				cats[name] = [item];
-				cats[name].sort_index = item.benchmark_index;
-			} else {
-				cats[name].push(item);
-			}
-		}
-		
-		var tmp = [];
-		for(var prop in cats) {
-			var sort_index = cats[prop].sort_index;
-			delete cats[prop].sort_index;
-			tmp.push({
-				key: prop,
-				value: cats[prop],
-				sort_index: sort_index,
-			});
-		}
-		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
-		// tmp.sort(function(a,b) {
-		// 	return a.sort_index < b.sort_index;
-		// });
-		// console.log(tmp);
-
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
-		}
-
-		return res[0];
-	};
-});
 
 app.filter('groupByFramework_keyed', function() {//grouping traces by framework keyed and returning  predefined order
 	var res = [];
 	
-	return function(arr) {
+	return function(arr, keyedFlag, n) {
 		if(arr === undefined)
 			return [];
 		var cats = {};
+		if(n === undefined) {
+			n = 0;
+		}
+		if(keyedFlag === undefined) {
+			keyedFlag = true;
+		}
 		var name;
 		for(var i = 0; i < arr.length; i++) {
 			var item = arr[i];
-			if(!item.keyed || item.summary) //only keyed results
+			if(item.summary) 
+				continue;
+			if(keyedFlag && !item.keyed)
+				continue;
+			if(!keyedFlag && item.keyed)
 				continue;
 			name = item['framework'];
 			if(!cats.hasOwnProperty(name)) {
@@ -137,30 +73,44 @@ app.filter('groupByFramework_keyed', function() {//grouping traces by framework 
 		}
 		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
 
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
+		if(!angular.equals(tmp, res[n]) ) {
+			res[n] = tmp;
 		}
 
-		return res[0];
+		return res[n];
 	};
 });
 
 app.filter('groupByBenchmark_keyed', function() {//grouping by benchmark keyed and returning  predefined order
 	var res = [];
 	
-	return function(arr) {
+	return function(arr, keyedFlag, n, memFlag) {
 		if(arr === undefined)
 			return [];
 		var cats = {};
-		var name;
+		if(n === undefined) {
+			n = 0;
+		}
+		var name, bench_descr;
 		for(var i = 0; i < arr.length; i++) {
 			var item = arr[i];
-			if(!item.keyed || item.summary) //only keyed results
+			if(item.summary  ) //only keyed results
 				continue;
+			if(keyedFlag && !item.keyed)
+				continue;
+			if(!keyedFlag && item.keyed)
+				continue;
+			if(memFlag && item.type === 'CPU')
+				continue;
+			if(!memFlag && item.type === 'MEM')
+				continue;
+
 			name = item['benchmark'];
+			bench_descr = item['bench_descr'];
 			if(!cats.hasOwnProperty(name)) {
 				cats[name] = [item];
 				cats[name].sort_index = item.benchmark_index;
+				cats[name].bench_descr = bench_descr;
 			} else {
 				cats[name].push(item);
 			}
@@ -169,35 +119,47 @@ app.filter('groupByBenchmark_keyed', function() {//grouping by benchmark keyed a
 		var tmp = [];
 		for(var prop in cats) {
 			var sort_index = cats[prop].sort_index;
+			var d = cats[prop].bench_descr;
 			delete cats[prop].sort_index;
+			delete cats[prop].descr;
 			tmp.push({
 				key: prop,
+				bench_descr: d,
 				value: cats[prop],
 				sort_index: sort_index,
 			});
 		}
 		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
 
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
+		if(!angular.equals(tmp, res[n]) ) {
+			res[n] = tmp;
 		}
 
-		return res[0];
+		return res[n];
 	};
 });
 
 app.filter('getSummaries_keyed', function() {//getting summaries for keyed results
 	var res = [];
 	
-	return function(arr) {
+	return function(arr, keyedFlag, n, memFlag) {
 		if(arr === undefined)
 			return [];
 		var cats = {};
 		var name;
 		for(var i = 0; i < arr.length; i++) {
 			var item = arr[i];
-			if(!item.summary || (item['framework'].indexOf('keyed') < 0)) //only keyed summaries
+			if(!item.summary) //only keyed summaries
 				continue;
+			if(keyedFlag && !item.keyed)
+				continue;
+			if(!keyedFlag && item.keyed)
+				continue;
+			if(memFlag && item.category !== 'memory')
+				continue;
+			if(!memFlag && item.category === 'memory')
+				continue;
+			
 			name = item['category'];
 			if(!cats.hasOwnProperty(name)) {
 				cats[name] = [item];
@@ -219,52 +181,13 @@ app.filter('getSummaries_keyed', function() {//getting summaries for keyed resul
 		}
 		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
 
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
+		if(!angular.equals(tmp, res[n]) ) {
+			res[n] = tmp;
 		}
 
-		return res[0];
+		return res[n];
 	};
 });
 
 
-app.filter('getSummaries_nonk', function() {//getting summaries for non-keyed results
-	var res = [];
-	
-	return function(arr) {
-		if(arr === undefined)
-			return [];
-		var cats = {};
-		var name;
-		for(var i = 0; i < arr.length; i++) {
-			var item = arr[i];
-			if(!item.summary || (item['framework'].indexOf('keyed') > 0)) //only non-keyed summaries
-				continue;
-			name = item['category'];
-			if(!cats.hasOwnProperty(name)) {
-				cats[name] = [item];
-				cats[name].sort_index = item.category_index;
-			} else {
-				cats[name].push(item);
-			}
-		}
-		
-		var tmp = [];
-		for(var prop in cats) {
-			var sort_index = cats[prop].sort_index;
-			delete cats[prop].sort_index;
-			tmp.push({
-				key: prop,
-				value: cats[prop],
-				sort_index: sort_index,
-			});
-		}
-		// tmp.sort((a,b) => {return a.sort_index > b.sort_index;})
 
-		if(!angular.equals(tmp, res[0]) ) {
-			res[0] = tmp;
-		}
-
-		return res[0];
-	};
-});
